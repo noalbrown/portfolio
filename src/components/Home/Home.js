27fed6dbd7e31f5da056e2b0dc8c3580cf2d38d2
Noal Brown <noalbrown1@gmail.com>
2020-10-07 15:58:22 -0500

    home page design

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import './home.css';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <ul>
+        <li className="effects"></li>
+        <li className="effects"></li>
+        <li className="effects"></li>
+        <li className="effects"></li>
+        <li className="effects"></li>
+        <li className="effects"></li>
+        <li className="effects"></li>
+        <li className="effects"></li>
+        <li className="effects"></li>
+        <li className="effects"></li>
+        <li className="effects"></li>
+      </ul>
+    </div>
   )
 }
 export default Home;
