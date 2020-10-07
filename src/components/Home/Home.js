819f9817624e75f67dddd0e06acefe87655de3e4
Noal Brown <noalbrown1@gmail.com>
2020-10-07 18:05:57 -0500

    Homepage Finished

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,7 +3,12 @@ import './home.css';
 
 const Home = () => {
   return (
-    <div>
+    <div className='home'>
+      <section>
+        <h1>NOAL BROWN</h1>
+        <br></br>
+        <h6>WEB DEVELOPER</h6>
+      </section>
       <ul>
         <li className="effects"></li>
         <li className="effects"></li>
