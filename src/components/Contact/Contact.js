7493d36aa15cf60e110abf1d1fd97e82f9476822
Noal Brown <noalbrown1@gmail.com>
2020-10-08 15:44:56 -0500

    contact page work

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './contact.css';
 
-const Contact = () => {
+const Contact = (props) => {
+
+  const logo = require('./favicon-32x32.png');
+
   return (
-    <div>Contact</div>
+    <div className='contact'>
+      <nav>
+        <img src={logo} alt='Logo' onClick={() => props.history.push('/')} />
+      </nav>
+    </div>
   )
 }
 export default Contact;
