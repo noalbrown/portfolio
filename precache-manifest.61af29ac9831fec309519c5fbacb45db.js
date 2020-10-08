b561e552985315488bda4e31a6c0a321495ba952
Noal Brown <noalbrown1@gmail.com>
2020-10-08 09:54:16 -0500

    Updates

---
+++ precache-manifest.61af29ac9831fec309519c5fbacb45db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eed3cf02a3a6ef6132563909f31c1955",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "afc5c93b670022641664",
+    "url": "/portfolio/static/css/main.d1a5ced8.chunk.css"
+  },
+  {
+    "revision": "658d8b95dbe8be6f87c2",
+    "url": "/portfolio/static/js/2.b98361fd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.b98361fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afc5c93b670022641664",
+    "url": "/portfolio/static/js/main.87d1e1cc.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.c80d6b77e76bda9b950f4251e17b94dd.js"
+  "/portfolio/precache-manifest.61af29ac9831fec309519c5fbacb45db.js"
 );
 
 self.addEventListener('message', (event) => {
