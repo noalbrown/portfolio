c487f02e485165a71392d7ea9a59fa25b60e7f32
Noal Brown <noalbrown1@gmail.com>
2020-10-08 13:02:41 -0500

    Home page finalized

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './home.css';
 
 const Home = () => {
   return (
     <div className='home'>
+      <nav>
+        <div>
+          <Link to='/about' className='links'>ABOUT</Link>
+        </div>
+        <div>
+          <Link to='/contact' className='links'>CONTACT</Link>
+        </div>
+      </nav>
       <section>
         <h1>NOAL BROWN</h1>
         <br></br>
