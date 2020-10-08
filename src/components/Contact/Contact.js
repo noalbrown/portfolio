9854e5b629c6acb541caf956f0b5b678c87a017e
Noal Brown <noalbrown1@gmail.com>
2020-10-08 18:46:33 -0500

    mail form

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
 import './contact.css';
 
 const Contact = (props) => {
+  const [toggle, setToggle] = useState(true);
 
   const logo = require('./favicon-32x32.png');
 
@@ -11,7 +11,53 @@ const Contact = (props) => {
       <section>
         <nav>
           <img src={logo} alt='Logo' onClick={() => props.history.push('/')} />
+          <div onClick={() => props.history.push('/about')}>ABOUT</div>
         </nav>
+        {toggle ? (
+          <div>
+            <button onClick={() => { setToggle(!toggle) }}>Contact Me</button>
+          </div>
+        ) : (
+            <form
+              action='mailto:noalbrown1@gmail.com'
+              method='get'>
+              <div>
+                <label for='name'>Name:
+                  <input
+                    type='text'
+                    name='name'
+                    id='name' />
+                </label>
+              </div>
+              <div>
+                <label for='email'>Email:
+                  <input
+                    type='text'
+                    name='email'
+                    id='email' />
+                </label>
+              </div>
+              <div>
+                <label>Message:</label>
+                <br />
+                <textarea name='comments'
+                  rows='12'
+                  cols='35'>
+                </textarea>
+              </div>
+              <div>
+                <input type='submit'
+                  name='submit'
+                  value='Send'
+                  onClick={() => { setToggle(!toggle) }} />
+                <input
+                  type='reset'
+                  name='reset'
+                  value='Cancel'
+                  onClick={() => { setToggle(!toggle) }} />
+              </div>
+            </form>
+          )}
       </section>
     </div>
   )
