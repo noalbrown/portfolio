ff3199fc1cbdff97a1465b71993da184a57dc69b
Noal Brown <noalbrown1@gmail.com>
2020-12-04 15:31:32 -0600

    bug fix

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -3,9 +3,11 @@ import './notFound.css'
 
 const NotFound = () => {
   return (
-    <div>
-      <h1>"Does Not Exist"</h1>
-      <strong>The page you are looking for does not exist</strong>
+    <div className="notFound">
+      <div className="container">
+        <h1>"Does Not Exist"</h1>
+        <strong>The page you are looking for does not exist</strong>
+      </div>
     </div>
   )
 }
