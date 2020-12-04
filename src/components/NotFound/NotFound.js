2ee011879e047213ef021a3ee99edf5ead7a6847
Noal Brown <noalbrown1@gmail.com>
2020-12-04 15:25:54 -0600

    added dead end page

---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './notFound.css'
+
+const NotFound = () => {
+  return (
+    <div>
+      <h1>"Does Not Exist"</h1>
+      <strong>The page you are looking for does not exist</strong>
+    </div>
+  )
+}
+
+export default NotFound
--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,13 @@ import { Switch, Route } from 'react-router-dom'
 import About from './components/About/About';
 import Contact from './components/Contact/Contact';
 import Home from './components/Home/Home';
+import NotFound from './components/NotFound/NotFound';
 
 export default (
   <Switch>
     <Route exact path='/' component={Home} />
     <Route exact path='/contact' component={Contact} />
     <Route exact path='/about' component={About} />
+    <Route component={NotFound} />
   </Switch>
 );
